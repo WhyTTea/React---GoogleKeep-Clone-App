cb1e7df6f974078693a71a9e45a0429c1fea6619
WhyTTea <teodorovych.yuriy@gmail.com>
2022-11-20 13:48:27 -0500

    Initial push with the setup and preliminary version of the UI

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+    return (<div className="note">
+                <h1>Section for title</h1>
+                    <p>Sub-section for content</p>
+            </div>
+    )
+}
+
+export default Note;
